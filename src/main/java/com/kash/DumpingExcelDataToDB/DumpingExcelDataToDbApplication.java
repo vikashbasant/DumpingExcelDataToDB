560f2aece859f8374a5d483b900c50bb78501b65
Vikash Basant <basantvikash360@gmail.com>
2023-07-13 09:31:13 +0530

    spring boot project dumping excel data into database.

---
+++ src/main/java/com/kash/DumpingExcelDataToDB/DumpingExcelDataToDbApplication.java
@@ -0,0 +1,13 @@
+package com.kash.DumpingExcelDataToDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumpingExcelDataToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumpingExcelDataToDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kash/DumpingExcelDataToDB/DumpingExcelDataToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kash.DumpingExcelDataToDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumpingExcelDataToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
